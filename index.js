798ab71eeedc6eda4513f760cbd41b360cbfcf81
Xiaobang Sun <xiaobang.sun@foxmail.com>
2023-03-27 02:33:23 +0300

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(express.json())
+
 
 morgan.token('body', function getBody (req) {
   //console.log(typeof req.body)
   return JSON.stringify(req.body)
 })
+app.use(cors())
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+//app.use(express.static('build'))
 
 let persons = [
   { 
@@ -88,7 +92,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
